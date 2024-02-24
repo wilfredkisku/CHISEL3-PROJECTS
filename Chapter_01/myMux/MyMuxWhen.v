e4c1569ea4ccadde662105cc5fdd3d3cca17bd54
Wilfred Kisku <wilfred.kisku@gmail.com>
2024-02-25 04:58:55 +0530

    Add files via upload

---
+++ Chapter_01/myMux/MyMuxWhen.v
@@ -0,0 +1,10 @@
+module MyMuxWhen(
+  input        clock,
+  input        reset,
+  input        io_s,
+  input  [7:0] io_in0,
+  input  [7:0] io_in1,
+  output [7:0] io_out
+);
+  assign io_out = io_s ? io_in1 : io_in0; // @[myMuxWhen.scala 13:15 14:12 16:12]
+endmodule
